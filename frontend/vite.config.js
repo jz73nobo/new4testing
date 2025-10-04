b082e8f9df3f5033dba564a9a44ad8a2760e9516
JinyuanZhang <jinyuan.zhang@stud.tu-darmstadt.de>
2025-10-05 00:12:57 +0200

    前端修改

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,7 @@
-// Vite的配置函数，提供类型提示和自动补全
 import { defineConfig } from 'vite'
-
-// Vite的React插件，支持JSX、热更新等React特性
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({   // 导出 Vite 配置对象
-    plugins: [react()],  // 启用React插件.JSX/TSX 文件编译;热模块替换
-    server: {
-        port: 5173,  // 前端运行在：http://localhost:5173
-        proxy: {
-            '/api': 'http://localhost:8080'  // 后端运行在：http://localhost:8080
-        }
-    }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
 })
